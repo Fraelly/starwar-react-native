a53ed76b4754b67afbe48ce920b6391796c6debd
Fraelly <fraelly@live.com>
2024-08-19 00:58:12 -0300

    estudos-1

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("Star War");
+  const [person, setPerson] = useState({ name: "Luker", mestre: "Yoda" });
+
+  function click() {
+    setName("Luke Filho");
+    setPerson({ name: "Veid", mestre: "Dark" });
+  }
+
+  // const click = () => setName("Yoda Mestre");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.top}>
+        <Text style={styles.tcolor}>Nome do aluno é {name}</Text>
+        <Text style={styles.tcolor}>
+          {person.mestre} é o mestre do {person.name}
+        </Text>
+        <View style={styles.button}>
+          <Button title="updata state" onPress={click} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +30,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tcolor: {
+    color: "#fff",
+    fontStyle: "italic",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  top: {
+    backgroundColor: "#005C53",
+  },
+  button: {
+    paddingTop: 20,
   },
 });
